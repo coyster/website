8a61706352fe1d965cb6cb641dcd591c6b4155d2
coyster <oyster.c@gmail.com>
2024-05-21 14:46:27 -0400

    initial

--- gotth_stack/cmd/app/go.mod
+++ gotth_stack/cmd/app/go.mod
@@ -1,3 +1,8 @@
 module app
 
 go 1.22.2
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- gotth_stack/cmd/app/main.go
+++ gotth_stack/cmd/app/main.go
@@ -1,9 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"github.com/go-chi/chi"
+
+	"github.com/oyste/gotth_stack/cmd/app/handlers"
+)
+
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	router := chi.NewMux()
+
+	router.Get("/", handlers.HandleFoo)
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+
 	fmt.Println("Heelo")
-	
-
-
+	slog.Info("Listening ")
+	http.ListenAndServe(listenAddr, router)
 
 }
